1d30b70c8aa2079681419af29a4b1da4375cd162
matheus-fls <matheus_fls@yahoo.com.br>
2019-12-03 20:34:36 -0300

    Basic home page and design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hellow!");
+import './styles.scss';
+
+document.getElementById('content').innerHTML = `
+<header>
+  <a href="http://localhost:8080/"><h1 class="logo">BRUTAL BURGERZ</h1></a>
+  <nav>
+    <ul class="nav_links">
+      <li><a href="#">MENUS</a></li>
+      <li><a href="#">ABOUT</a></li>
+    </ul>
+  </nav>
+  <a href="#"><button>CONTACT</button></a>
+</header>
+<main>
+  <div class="bg-container">
+    <div class="content">
+      <h1 class="main-desc">Monster burgers for your daily brutal needs!</h1>
+    </div>
+  </div>
+</main>
+`
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,25 @@ module.exports = {
   output: {
     filename: 'main.js',
     publicPath: 'dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }
+    ]
   }
 };
